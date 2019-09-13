c867019ba9f54ace163ef497cab7f3c16d3e5d8a
Enmanuelolivo <45018826+Enmanuelolivo@users.noreply.github.com>
2019-09-13 16:58:22 -0400

    Initial Commit

---
+++ Api/Api/Api/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Api.Services;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Api
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            await CallApi();
+        }
+        async Task CallApi()
+        {
+            var nsAPI = RestService.For<IApiServices>("https://maps.googleapis.com");
+            var restaurants = await nsAPI.getNearBySearch("18.491955, -69.93689");
+        }
+    }
+}
---
+++ Api/Api/Api/Services/IApiServices.cs
@@ -0,0 +1,15 @@
+﻿using Api.Model;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Services
+{
+    public interface IApiServices
+    {
+        [Get("/api/place/nearbysearch/json?location=18.491955, -69.936898&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyBoI3qILgCG5LRu0PAl2Vmz_GdkBROlImE")]
+        Task<Restaurants> getNearBySearch(string Results);
+    }
+}
